dcc07ab5db8f33bfd4af7c4beeb509d34caecaa6
spheroidon <spheroidon@gmail.com>
2024-01-01 20:11:14 +0200

    secanaas

--- app.py
+++ app.py
@@ -1,7 +1,14 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, send_from_directory, url_for
 import sqlite3
+from werkzeug.utils import secure_filename
+import os
 
 app = Flask(__name__)
+
+UPLOAD_FOLDER = 'uploads'  # Set your desired upload folder
+
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
 
 # Function to create the database table if it doesn't exist
 def create_table():
@@ -31,17 +38,28 @@ def index():
     conn.close()
     return render_template('index.html', animals=animals)
 
-# Route to add a new animal
+@app.route('/uploads/<filename>')
+def uploaded_file(filename):
+    return send_from_directory(app.config['UPLOAD_FOLDER'], filename)
+
+
 @app.route('/add', methods=['POST'])
 def add_animal():
     name = request.form['name']
     species = request.form['species']
     age = request.form['age']
-    photo = request.form['photo']
+    photo = request.files['photo']
+
+    if photo:
+        filename = secure_filename(photo.filename)
+        photo.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+        photo_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+    else:
+        photo_path = None
 
     conn = sqlite3.connect('animals.db')
     cursor = conn.cursor()
-    cursor.execute('INSERT INTO animals (name, species, age, photo) VALUES (?, ?, ?, ?)', (name, species, age, photo))
+    cursor.execute('INSERT INTO animals (name, species, age, photo) VALUES (?, ?, ?, ?)', (name, species, age, photo_path))
     conn.commit()
     conn.close()
 
@@ -68,21 +86,33 @@ def edit_animal(animal_id):
     conn.close()
     return render_template('edit.html', animal=animal)
 
-# Route to update a animal after editing
 @app.route('/update/<int:animal_id>', methods=['POST'])
 def update_animal(animal_id):
     name = request.form['name']
     species = request.form['species']
     age = request.form['age']
-    photo = request.form['photo']
+    new_photo = request.files['photo']
 
     conn = sqlite3.connect('animals.db')
     cursor = conn.cursor()
-    cursor.execute('UPDATE animals SET name = ?, species = ?, age = ?, photo = ? WHERE id = ?', (name, species, age, photo, animal_id))
+    cursor.execute('SELECT * FROM animals WHERE id = ?', (animal_id,))
+    animal = cursor.fetchone()
+
+    if new_photo:
+        # If a new photo is uploaded, handle it similar to adding a new animal's photo
+        filename = secure_filename(new_photo.filename)
+        new_photo.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+        photo_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+    else:
+        # If no new photo uploaded, keep the old photo path
+        photo_path = animal[4]
+
+    cursor.execute('UPDATE animals SET name = ?, species = ?, age = ?, photo = ? WHERE id = ?', (name, species, age, photo_path, animal_id))
     conn.commit()
     conn.close()
 
     return redirect(url_for('index'))
+
 
 if __name__ == '__main__':
     app.run(debug=True)
